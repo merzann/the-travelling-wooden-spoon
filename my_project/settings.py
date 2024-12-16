cc5e20c3d99282ba0031b49afa048da5ea714f6a
merzann <annisch78@gmail.com>
2024-12-16 19:17:02 +0000

    fix: delete wildcard from .herokuapp.com

--- my_project/settings.py
+++ my_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-merzann-thetravellingwo-5t97fn2n60b.ws-eu117.gitpod.io', '*.herokuapp.com']
+ALLOWED_HOSTS = ['8000-merzann-thetravellingwo-5t97fn2n60b.ws-eu117.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
