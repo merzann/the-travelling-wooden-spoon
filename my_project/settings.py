0dc53c1d4b6892e60187d8ba686a702fa0584626
merzann <annisch78@gmail.com>
2024-12-17 02:54:22 +0000

    feat: instal whitenoise 6.5.0, update settings.py

--- my_project/settings.py
+++ my_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -91,7 +92,7 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "https://https://8000-merzann-thetravellingwo-5t97fn2n60b.ws-eu117.gitpod.io/",
     "https://*.codeinstitute-ide.net/",
-    "*https://*.herokuapp.com/",
+    "https://*.herokuapp.com/",
 ]
 
 
@@ -130,6 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
