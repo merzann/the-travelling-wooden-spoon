7c3a8849cd9cb913162ff0365134266b9114013e
merzann <annisch78@gmail.com>
2024-12-17 01:36:16 +0000

    maint: registered new models in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Recipe
+from .models import Category, Recipe, HomepageFeature, BlogPost
 
 # RecipeAdmin with filtering and search
 @admin.register(Recipe)
@@ -9,8 +9,24 @@ class RecipeAdmin(admin.ModelAdmin):
     search_fields = ('title', 'description')  # Enable search
     prepopulated_fields = {'excerpt': ('description',)}  # Auto-generate excerpt from description
 
+
 # CategoryAdmin for managing recipe categories
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+
+
+# HomepageFeatureAdmin for managing featured recipes
+@admin.register(HomepageFeature)
+class HomepageFeatureAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'excerpt')
+    search_fields = ('recipe__title',)
+
+
+# BlogPostAdmin for managing blog posts
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
+    search_fields = ('title', 'snippet')
+    ordering = ('-date',)
