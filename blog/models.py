9d14d8ad5a9255f41e7045dee4833996936111af
merzann <annisch78@gmail.com>
2024-12-18 00:18:55 +0000

    feat: add model class Rating, amend model calss Recipe to incorporate rating, add calculate_average_ratin function to recipe model to track ratings

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,11 @@ class Category(models.Model):
         return self.name
 
 
-# Recipe model to store details of each recipe
+# Recipe model
+"""
+stores all information about recipes
+tracks thumbs-up/thumbs-down ratings
+"""
 class Recipe(models.Model):
     STATUS = (
         (0, "Draft"),
@@ -20,15 +24,37 @@ class Recipe(models.Model):
 
     title = models.CharField(max_length=200)
     description = models.TextField()
-    excerpt = models.TextField(max_length=300, blank=True, null=True)  # For homepage
+    excerpt = models.TextField(max_length=300, blank=True, null=True)
     image = CloudinaryField('image', null=True, blank=True)
     category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, related_name='recipes')
     date = models.DateTimeField(auto_now_add=True)
     popularity_score = models.IntegerField(default=0)
-    status = models.IntegerField(choices=STATUS, default=0)  # Draft by default
+    status = models.IntegerField(choices=STATUS, default=0)
+    thumbs_up = models.PositiveIntegerField(default=0)
+    thumbs_down = models.PositiveIntegerField(default=0)
+
+    def calculate_average_rating(self):
+        """function to track rating"""
+        ratings = self.ratings.all()
+        if ratings.exists():
+            total_score = sum(rating.score for rating in ratings)
+            return total_score / ratings.count()
+        return 0
+
+    def total_votes(self):
+        return self.ratings.count()
 
     def __str__(self):
         return self.title
+
+
+# Star rating 1–5
+class Rating(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name='ratings', on_delete=models.CASCADE)
+    score = models.PositiveSmallIntegerField(choices=[(i, i) for i in range(1, 6)])
+
+    def __str__(self):
+        return f'{self.recipe.title} - {self.score}'
 
 
 # HomepageFeature model to manage featured recipes displayed on homepage
--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     # Homepage URL
     path('', views.homepage, name='home'),
+    path('category/<str:category_name>/', views.category_view, name='category_view'),
 ]
